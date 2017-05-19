e74f560de07527d2e3141b5f22bee1404c667deb
Lukas Einfalt <lukas.einfalt@daimler.com>
2017-05-19 14:09:44 +0200

    added html, favicon

--- webpack-react-redux-sass/webpack.config.js
+++ webpack-react-redux-sass/webpack.config.js
@@ -2,22 +2,30 @@
 
 var path = require('path');
 var webpack = require('webpack');
+var HtmlPlugin = require('html-webpack-plugin');
+var FaviconPlugin = require('favicons-webpack-plugin');
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
     devtool: 'eval-source-map',
-    entry: [
-        path.join(__dirname, 'src/app.js')
-    ],
+    entry: {
+        main: path.join(__dirname, 'src/app.js')
+    },
     output: {
         path: path.join(__dirname, '/dist/'),
-        filename: '[name].js',
+        filename: '[name].[chunkhash].js',
         publicPath: '/'
     },
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-        })
+        }),
+        new HtmlPlugin({
+            template: path.join(__dirname, 'src/index.html.ejs'),
+            title: "This title is set in webpack.config.js and should be changed."
+        }),
+        new FaviconPlugin(path.join(__dirname, 'src/img/favicon.png'))
     ],
     module: {
         loaders: [
@@ -30,8 +38,8 @@ module.exports = {
                 test: /\.scss$/,
                 loader: 'style-loader!css-loader!sass-loader'
             },
-            { test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-            { test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/, loader: 'file' }
+            {test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+            {test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/, loader: 'file'}
         ]
     }
 };
